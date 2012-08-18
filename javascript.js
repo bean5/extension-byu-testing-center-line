a44d491e8547f2e5b2c1e7a4139895c9b569b050
unknown <bean5@win7.(none)>
2012-08-18 15:34:48 -0600

    Prepared for future changes (addition of href)

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
-setInterval(updateTestingCenter, 7 * 1000);
-
-var d = new Date();
+setInterval(updateTestingCenter, 100);
 
 function updateTestingCenter()
 {
 	document.getElementById("testing_center").src = "";
 	document.getElementById("testing_center").src = "http://testing.byu.edu/images/conditions/axiscam.jpg";
+}
+
+function openTab(url)
+{
+	//alert(url);
+	//window.location = url;
+	chrome.tabs.create({'url': url}, function(tab) {
+	  // Tab opened.
+	});
 }
